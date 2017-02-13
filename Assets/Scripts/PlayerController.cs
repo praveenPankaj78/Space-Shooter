6a9e56e1b40c2ef8bcefa5da2d7d17ca3f109721
Praveen Kumar Pankaj <Praveen Kumar Pankaj>
2017-02-14 01:16:35 +0530

    Added sound effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
     public GameObject shot;
     public Transform shotSpawn;
     public float fireRate;
+    public AudioClip a;
 
     private float nextFire;
 
@@ -32,6 +33,7 @@ public class PlayerController : MonoBehaviour
         {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
         }
     }
 
